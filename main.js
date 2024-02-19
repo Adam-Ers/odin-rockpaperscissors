e2599e7a0aceca11173d5cd9e0b5f741310166aa
Adam Erstad <adamerstad@gmail.com>
2024-02-19 03:53:41 -0800

    Basic RPS function/game added with no loop (Step 4)

--- main.js
+++ main.js
@@ -1,7 +1,39 @@
-const rpsChoices = ["Rock", "Paper", "Scissors"]
+const rpsChoices = ["Rock", "Paper", "Scissors"];
+const playerChoices = ["r", "p", "s"];
 
 function getComputerChoice() {
-    return rpsChoices[Math.floor(Math.random() * (rpsChoices.length))]
+    return rpsChoices[Math.floor(Math.random() * (rpsChoices.length))];
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    let choice = playerSelection
+    if (choice == undefined) {choice = "undefined";}
+    let computerChoice = computerSelection
+    let choiceLetter = choice.toLowerCase();
+    choiceLetter = choiceLetter.charAt(0);
+    if(playerChoices.find(element => element == choiceLetter) == undefined)
+    {
+        return "Invalid choice!"
+    }
+    else
+    {
+        let choiceNumber = playerChoices.findIndex(element => element == choiceLetter);
+        let computerChoiceNumber = rpsChoices.findIndex(element => element == computerChoice)
+        if (choiceNumber == 2 && computerChoiceNumber == 0) { computerChoiceNumber = 3;} // If we got scissors and they got rock, make them higher in number.
+        if (choiceNumber == 0 && computerChoiceNumber == 2) { choiceNumber = 3; } // Vice versa for the opposite situation.
+        if (choiceNumber > computerChoiceNumber)
+        {
+            return `You win! ${choice} beats ${computerChoice}.`;
+        }
+        else if (choiceNumber < computerChoiceNumber)
+        {
+            return `You lose. ${computerChoice} beats ${choice}.`;
+        }
+        else
+        {
+            return `Tie. Both of you picked ${choice}.`;
+        }
+    }
+}
+
+console.log(playRound(prompt("Choose your move:"), getComputerChoice()));
